d16748eb6bec945c5fb0ed524cd3ad0375726015
Surbhi-SD <99402618+Surbhi-SD@users.noreply.github.com>
2022-07-02 16:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-// Make an keyless car EVEN BETTER!
-// We are improving our car from previous exercise now. 
 
 
-var age = prompt("What is your age?");
-
-if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
-} else if (Number(age) > 18) {
-	alert("Powering On. Enjoy the ride!");
-} else if (Number(age) === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
-}
-
-//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+//1. Make the code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
 function checkDriverAge() {
 	var age = prompt("What is your age?");
 	if (Number(age) < 18) {
@@ -23,7 +11,7 @@ function checkDriverAge() {
 		alert("Congratulations on your first year of driving. Enjoy the ride!");
 	}
 }
-// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime.
 
 
 //2. Create another function that does the same thing, assign it to checkDriverAge2 variable using Function Expression.
